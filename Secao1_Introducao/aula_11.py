77f3a8e32a46492d3bea0045b1d5c06bc2671eaa
Pedro Artur <pedroartur25@gmail.com>
2024-10-28 11:29:43 -0300

    Exericio Exceptions

--- Secao1_Introducao/aula_11.py
+++ Secao1_Introducao/aula_11.py
@@ -1,12 +1,15 @@
-nome = input('Digite seu primeiro nome: ')
-tamanho_nome = len(nome)
+try:
+    nome = input('Digite seu primeiro nome: ')
+    tamanho_nome = len(nome)
 
-if tamanho_nome > 1:
-    if tamanho_nome <= 4:
-        print('Seu nome é curto')
-    elif tamanho_nome == 5 or tamanho_nome == 6:
-        print('Seu nome é normal')
-    elif tamanho_nome > 6:
-        print('Seu nome é muito grande')
-else:
-    print('Digite mais de uma letra.') 
+    if tamanho_nome > 1:
+        if tamanho_nome <= 4:
+            print('Seu nome é curto')
+        elif tamanho_nome == 5 or tamanho_nome == 6:
+            print('Seu nome é normal')
+        elif tamanho_nome > 6:
+            print('Seu nome é muito grande')
+    else:
+        print('Digite mais de uma letra.')
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")

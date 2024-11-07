0a421f89039945253207884da9685ad14ba187c2
Pedro Artur <pedroartur25@gmail.com>
2024-11-07 10:23:00 -0300

    Validador de CPF

--- Secao1_Introducao/aula_35.py
+++ Secao1_Introducao/aula_35.py
@@ -1,4 +1,7 @@
-cpf_enviado_usuario = '74682489070'
+import re
+
+entrada = input('CPF[746.824.890-70]: ')
+cpf_enviado_usuario = re.sub(r'[^0-9]', '', entrada)
 nove_digitos = cpf_enviado_usuario[:9]
 contador_regressivo_1 = 10
 
